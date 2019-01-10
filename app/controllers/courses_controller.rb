f5143733d67b20bc2779c813d2cd78d06e83e913
Rashell Marcelino <rashellmarcelino@Rashells-MacBook-Air.local>
2019-01-09 23:46:02 -0500

    courses controller/ with delete method

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,22 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def create
+    Course.create(subject: params[:subject], time_completion: params[:time_completion], description: params[:describe])
+    redirect_to '/courses'
+  end
+
+  def edit
+  end
+
+  def destroy
+    @courses = Course.find(params[:id])
+      @courses.destroy
+      redirect_to '/courses'
+  end
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,31 @@
 Rails.application.routes.draw do
+  # get 'courses/index'
+  # get 'courses/create'
+  # get 'courses/edit'
+  # get 'courses/destroy'
   # get 'cohorts/index'
   # get 'cohorts/create'
   # get 'cohorts/edit'
   # get 'cohorts/destroy'
   
+  # cohorts__________
   root to: 'cohorts#index'
   post '/cohorts' => 'cohorts#create'
   get '/cohorts/:id' => 'cohorts#destroy'
-
-
   get '/cohorts' =>'cohorts#index'
+
+# courses___________
   get '/courses' =>'courses#index'
+  post '/courses' => 'courses#create'
+  get '/courses/:id' => 'courses#destroy'
+
+
+  # students__________
   get '/students' =>'students#index'
+
+
+
+  # teachers__________
   get '/teachers' =>'teachers#index'
 
 
